ff1c892b134f3f2f544fb3405ac8909ee1401f7a
Shanuka <shanukaprabodha1262@gmail.com>
2021-06-06 14:04:39 +0530

    4nd

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-require("dotenv").config();
-
-const PORT = process.env.PORT || 8270;
-
-const session = require("express-session");
-
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8270;
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
@@ -35,12 +31,6 @@ connection.once("open",()=>{
 const PaymentRouter = require("./src/routes/AtmUser");
 app.use("/atm",PaymentRouter);
 
-const studentRouter = require("./src/routes/students.js");
-app.use("/student",studentRouter);
-
-const UserRouter = require("./src/routes/signin.js");
-app.use("/student",UserRouter);
-
 const fileRoute = require("./src/routes/file");
 app.use(fileRoute);
 
@@ -53,14 +43,12 @@ app.get('*', (request, response) => {
 });
 
 
-
 /*
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 */
-
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}`)
 });
